d48821824777250b8df46e928319b83ff71229e9
Marcel Haberl <srsyntaxat@gmail.com>
2022-10-06 21:34:48 +0200

    Changed license in newer classes, configuration fixes and replace & in error messages in modify command.

--- src/main/java/at/srsyntax/tabstatistic/config/PluginConfig.java
+++ src/main/java/at/srsyntax/tabstatistic/config/PluginConfig.java
@@ -6,24 +6,27 @@ import java.io.File;
 import java.io.IOException;
 
 /*
- * CONFIDENTIAL
- *  Unpublished Copyright (c) 2022 Marcel Haberl, All Rights Reserved.
+ * MIT License
  *
- * NOTICE:
- * All information contained herein is, and remains the property of Marcel Haberl. The intellectual and
- * technical concepts contained herein are proprietary to Marcel Haberl and may be covered by U.S. and Foreign
- * Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this
- * information or reproduction of this material is strictly forbidden unless prior written permission is obtained
- * from Marcel Haberl.  Access to the source code contained herein is hereby forbidden to anyone without written
- * permission Confidentiality and Non-disclosure agreements explicitly covering such access.
+ * Copyright (c) 2022 Marcel Haberl
  *
- * The copyright notice above does not evidence any actual or intended publication or disclosure  of  this source code,
- * which includes information that is confidential and/or proprietary, and is a trade secret, of Marcel Haberl.
- * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS
- * SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF Marcel Haberl IS STRICTLY PROHIBITED, AND IN VIOLATION OF
- * APPLICABLE LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED
- * INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO
- * MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 public class PluginConfig {
 
@@ -49,11 +52,12 @@ public class PluginConfig {
             configuration.addDefault("message.noPermission", "&cYou have no rights to do this.");
             configuration.addDefault("message.useage", "&cUseage&8: &f/modifystatistic <player> <statistic> <value> <qualifier>");
             configuration.addDefault("message.not.found.player", "&cPlayer not found.");
-            configuration.addDefault("message.not.found.stastistic", "&cStatistic not found.");
+            configuration.addDefault("message.not.found.statistic", "&cStatistic not found.");
             configuration.addDefault("message.not.found.entityType", "&cEntityType not found.");
             configuration.addDefault("message.not.found.material", "&cMaterial not found.");
             configuration.addDefault("message.needQualifier", "&cYou need a %s qualifier.");
             configuration.addDefault("message.invalidValue", "&cInvalid value. The value must be an integer.");
+            configuration.options().copyDefaults(true);
             configuration.save(file);
         }
 
@@ -64,7 +68,7 @@ public class PluginConfig {
                         configuration.getString("message.noPermission"),
                         configuration.getString("message.useage"),
                         configuration.getString("message.not.found.player"),
-                        configuration.getString("message.not.found.stastistic"),
+                        configuration.getString("message.not.found.statistic"),
                         configuration.getString("message.not.found.entityType"),
                         configuration.getString("message.not.found.material"),
                         configuration.getString("message.needQualifier"),
